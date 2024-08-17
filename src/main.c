4828befeecf19e18e2d7575e50b1121b2f0dc5ae
shivenBajpai <shiven.bajpai@gmail.com>
2024-08-17 18:53:41 +0530

    Cleaned up unused code

--- src/main.c
+++ src/main.c
@@ -2,61 +2,10 @@
 #include <stdbool.h>
 #include "index.h"
 
-void process_line(char *in_buf, char *out_buf, label_index* index, int* linecount, int* instr_count) {
-	bool lw_flag = true;
-	bool cw_flag = false;
-	bool instr_flag = true;
-	char* out_buf_base = out_buf;
-
-	do {
-		switch (*in_buf) {
-			case '#':
-				*(out_buf) = '\0';
-				instr_count += instr_flag?1:0;
-				return;
-			case ':':
-				if (lw_flag) printf("WARN: Empty Label on line %d, Ignoring and moving on...", *linecount);
-				else {	
-					*(out_buf) = '\0';
-					add_label(index, out_buf, *instr_count);
-					out_buf=out_buf_base;
-					instr_flag = false;
-				}
-				break;
-			case ' ':
-			case '\t':
-				if (lw_flag || cw_flag) break;
-				else cw_flag = true;
-			default:
-				lw_flag = false;
-				cw_flag = false;
-				instr_flag = true;
-				*(out_buf++) = *in_buf;
-		}
-		in_buf++;
-	} while (*in_buf != '\0');
-
-	instr_count += instr_flag?1:0;
-	return;
-}
-
 void process_input(FILE *in_fp, FILE *out_fp, label_index* index) {
+	char c;
 	int linecount = 1;
 	int instruction = 0;
-	
-	// char rawbuffer[1024];
-	// char linebuffer[1024];
-
-	// while (fgets(rawbuffer, sizeof linebuffer, in_fp)) {
-	// 	linecount++;
-	// 	if (*rawbuffer == '\n') continue;
-		
-	// 	process_line(rawbuffer, linebuffer, index, &linecount, &instruction);
-
-	// 	if (*linebuffer != '\0') fprintf(out_fp, "%s", linebuffer);
-	// }
-
-	char c;
 	int line_len = 0;
 	bool comment_flag = false;
 	bool lw_flag = true;
